a41ffd97a226eaebe2379358a02532bc25ebcefd
Chhavi <chhavi@vinsol.com>
2013-11-29 17:06:48 +0530

    code optimized

--- occurence_hash/lib/alphabet_occurence.rb
+++ occurence_hash/lib/alphabet_occurence.rb
@@ -1,8 +1,7 @@
 class AlphabetOccurence
   def count(input_str)
-    input_str = input_str.upcase
     string_hash = Hash.new(0)
-    input_str.each_char { |char| string_hash[char] += 1 if char =~ /[\w]/i }
+    input_str.upcase.each_char { |char| string_hash[char] += 1 if char =~ /[A-Z]/ }
     string_hash
   end
 end
