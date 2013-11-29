2efdf086ab8253dccdb1db26a6af3b8a831b2375
Chhavi <chhavi@vinsol.com>
2013-11-29 17:20:44 +0530

    indentation corrected

--- array_hash/bin/main.rb
+++ array_hash/bin/main.rb
@@ -1,5 +1,4 @@
 require_relative '../lib/array'
 
-array = ['abc','def',1234,234,'abcd','x','mnop',5,'zZzZ']
-puts "Length of each element in #{ array }"
+array = ['abc', 'def', 1234, 234, 'abcd', 'x', 'mnop', 5, 'zZzZ']
 puts array.to_length_hash
