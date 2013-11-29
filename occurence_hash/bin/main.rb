7a89686ad2805e8c085001fcc77357c26251ca3d
Chhavi <chhavi@vinsol.com>
2013-11-29 15:16:05 +0530

    function name changed, input extracted in a variable

--- occurence_hash/bin/main.rb
+++ occurence_hash/bin/main.rb
@@ -1,5 +1,6 @@
 require_relative '../lib/alphabet_occurence'
 
 puts 'Enter a String'
+input_string = gets.chomp
 alphabetOccurence = AlphabetOccurence.new
-alphabetOccurence.count_alphabet_occurrence(gets.chomp).each { |key, value| puts "#{ key } : #{ value }" }
+alphabetOccurence.count(input_string).each { |key, value| puts "#{ key } : #{ value }" }
--- occurence_hash/lib/alphabet_occurence.rb
+++ occurence_hash/lib/alphabet_occurence.rb
@@ -1,8 +1,8 @@
 class AlphabetOccurence
-  def count_alphabet_occurrence(input_str)
+  def count(input_str)
     input_str = input_str.upcase
     string_hash = Hash.new(0)
-    input_str.each_char { |char| string_hash[char] += 1 }
+    input_str.each_char { |char| string_hash[char] += 1 if char =~ /[\w]/i }
     string_hash
-  end 
+  end
 end
