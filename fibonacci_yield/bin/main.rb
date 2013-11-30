3844d69d6f000d6816d8f4cef974afd2153f6613
Chhavi <chhavi@vinsol.com>
2013-11-30 11:54:07 +0530

    function name changed

--- fibonacci_yield/bin/main.rb
+++ fibonacci_yield/bin/main.rb
@@ -2,5 +2,5 @@ require_relative '../lib/fibonacci'
 
 puts "Fibonacci-Series\n"
 fibonacci = Fibonacci.new
-fibonacci.series_up_to(1000) { |current_val| print current_val, " " }
+fibonacci.series_upto(1000) { |current_val| print current_val, " " }
 puts "\n"
--- fibonacci_yield/lib/fibonacci.rb
+++ fibonacci_yield/lib/fibonacci.rb
@@ -1,6 +1,6 @@
 class Fibonacci
-  def series_up_to(max_val)
-    current_val, next_val = 0, 1 
+  def series_upto(max_val)
+    current_val, next_val = 0, 1
     while current_val <= max_val
       yield current_val
       current_val, next_val = next_val, current_val + next_val
