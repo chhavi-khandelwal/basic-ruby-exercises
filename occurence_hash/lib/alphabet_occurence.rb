46fd9695f4b30000bce3a87f47fc5c564186fdc3
Chhavi <chhavi@vinsol.com>
2013-11-30 11:22:57 +0530

    code optimized using gsub

--- occurence_hash/lib/alphabet_occurence.rb
+++ occurence_hash/lib/alphabet_occurence.rb
@@ -1,7 +1,7 @@
 class AlphabetOccurence
   def count(input_str)
     string_hash = Hash.new(0)
-    input_str.upcase.each_char { |char| string_hash[char] += 1 if char =~ /[A-Z]/ }
+    input_str.upcase.gsub(/[A-Z]/i) { |char| string_hash[char] += 1 }
     string_hash
   end
 end
