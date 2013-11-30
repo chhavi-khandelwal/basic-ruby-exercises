78c8a73672891ab26c4037bfd54a628b42e5939c
Chhavi <chhavi@vinsol.com>
2013-11-30 12:45:47 +0530

    file removed

--- occurence_hash/lib/alphabet_occurence.rb
+++
@@ -1,7 +0,0 @@
-class String
-  def count_alphabet_occurence
-    alphabet_occurence_collection = Hash.new(0)
-    upcase.gsub(/[A-Z]/) { |char| alphabet_occurence_collection[char] += 1 }
-    alphabet_occurence_collection
-  end
-end
