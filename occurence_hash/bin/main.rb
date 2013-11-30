8e8353125d714960a0d8f6a0823d5fdcc2f8c98f
Chhavi <chhavi@vinsol.com>
2013-11-30 12:53:21 +0530

    function name modified

--- occurence_hash/bin/main.rb
+++ occurence_hash/bin/main.rb
@@ -1,5 +1,5 @@
 require_relative '../lib/alphabet_occurence'
 
-puts 'Enter a String'
+puts 'Enter a string'
 input_string = gets.chomp
 input_string.count_alphabet_occurence.each { |key, value| puts "#{ key } : #{ value }" }
