0812782ddc9dfd028bf716cf24f95d14c69937a7
Chhavi Khandelwal <chhavi@vinsol.com>
2013-12-03 12:03:25 +0530

    method name modified

--- hash_inject/bin/main.rb
+++ hash_inject/bin/main.rb
@@ -1,4 +1,4 @@
 require_relative '../lib/array'
 
 array = ['abc', 'def', 1234, 234, 'abcd', 'x', 'mnop', 5, 'zZzZ']
-puts array.group_by_even_and_odd_lengths
+puts array.group_by_even_and_odd_length
