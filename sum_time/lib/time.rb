3b5a03e0759f45c8bd71d6337ca14c0a9bccec32
Chhavi Khandelwal <chhavi@vinsol.com>
2013-12-05 11:26:41 +0530

    Solution to sum_time

--- sum_time/lib/time.rb
+++ sum_time/lib/time.rb
@@ -2,7 +2,7 @@ class Time
   def add_to(time2)
     time2_in_secs = time2.hour * 3600 + time2.min * 60 + time2.sec
     time = self + time2_in_secs
-    days = time.day - self.day
-    puts "days:#{ days } hour:#{ time.hour } min:#{ time.min } sec:#{ time.sec } "
+    day = time.day - self.day
+    puts "(#{ self.hour }:#{ self.min }:#{ self.sec },#{ time2.hour }:#{ time.min }:#{ time.sec }) -> #{ day == 0 ? '' : "#{ day } day &" } #{ time.hour }:#{ time.min }:#{ time.sec }"
   end
 end
