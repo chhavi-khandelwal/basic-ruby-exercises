3f4efbb5e96e644a9c56a1745efce968a8d6e4de
Chhavi Khandelwal <chhavi@vinsol.com>
2013-12-05 11:37:04 +0530

    {} and yield removed

--- highlight_search_result/lib/string.rb
+++ highlight_search_result/lib/string.rb
@@ -1,7 +1,7 @@
 class String
   def search_text(pattern)
     word_occurence = 0
-    highlighted_string = gsub(/#{pattern}/i) do |match|
+    highlighted_string = gsub(/#{ pattern }/i) do |match|
       word_occurence += 1
       match.replace "(#{ match })"
     end
