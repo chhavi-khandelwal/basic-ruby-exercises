e8a22812e8ed68aeb4c821ad2f05f4aba32445b1
Chhavi Khandelwal <chhavi@vinsol.com>
2013-12-06 13:00:12 +0530

    amount = nil case added, class variable name changed, refractoring done

--- customer_account_balance/lib/customer.rb
+++ customer_account_balance/lib/customer.rb
@@ -1,45 +1,45 @@
 class Customer
-  @@auto_incremented_id = 0
+  @@account_no = 0
 
   def initialize(name)
     @name = name
-    @@auto_incremented_id += 1
-    @account_no = @@auto_incremented_id
+    @@account_no += 1
+    @account_no = @@account_no
     @balance = 1000
   end
 
   def deposit(amount)
-    case
-     when amount == 0
-      print_transaction_message('amount=0', amount)
+    if amount.to_f <= 0
+      print_error_message('amount=0/nil')
     else
       @balance += amount
       print_transaction_message('deposited', amount)
-     end
+    end
     @balance
   end
 
   def withdraw(amount)
-    case
-    when amount == 0
-      print_transaction_message('amount=0', amount)
-    when @balance >= amount
+    if amount.to_f <= 0
+      print_error_message('amount=0/nil')
+    elsif @balance >= amount
       @balance -= amount
       print_transaction_message('withdrawn', amount)
     else
-      print_transaction_message('insufficient', amount)
+      print_error_message('insufficient')
     end
     @balance
   end
 
   def print_transaction_message(action, amount)
+    puts "amount #{ action } by #{ @name } with account no. #{ @account_no } is #{ amount }, balance = #{ @balance }"
+  end
+
+  def print_error_message(action)
     case action
     when 'insufficient'
-      puts "#{ action } balance in account no. #{ @account_no }"
-    when 'amount=0'
-      puts "amount should be greater than #{ amount }"
-    else
-      puts "amount #{ action } by #{ @name } with account no. #{ @account_no } is #{ amount }, balance = #{ @balance }"
+      puts "#{ action } balance in the account"
+    when 'amount=0/nil'
+      puts 'Enter a valid amount'
     end
   end
 end
