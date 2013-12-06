fed1dd90b151c4e245688837e848678515ef8b86
Chhavi Khandelwal <chhavi@vinsol.com>
2013-12-06 12:33:08 +0530

    removed upcase method

--- palindrome/bin/main.rb
+++ palindrome/bin/main.rb
@@ -1,10 +1,15 @@
 require_relative '../lib/string'
 
+quit_regexp = /^q$/i
+trimmed_string_regexp = /^\s*$/
 loop do
   puts 'Enter the string'
   input_string = gets.chomp
-  if input_string =~ /^q$/i
+  case input_string
+  when quit_regexp
     break
+  when trimmed_string_regexp
+    puts 'Enter a valid string'
   else
     puts "#{ input_string } is #{ input_string.palindrome?  ? '' : 'not' } a Palindrome"
   end
