9aaba76de4bfb744f53d6abd08f779ee5481bb46
Chhavi Khandelwal <chhavi@vinsol.com>
2013-12-09 12:58:36 +0530

    variable names modified

--- interest_difference/lib/interest.rb
+++ interest_difference/lib/interest.rb
@@ -1,18 +1,19 @@
 class Interest
-  def initialize(p, t, &block)
-    @p, @t, @block = p.to_i, t.to_i, block
-    @r = 0.1
+  RATE = 0.1
+
+  def initialize(principal, time, &block)
+    @principal, @time, @block = principal.to_i, time.to_i, block
   end
 
-  def calculate_simply
-    simple_interest = @p * @r * @t
+  def get_simple_interest
+    simple_interest = @principal * RATE * @time
   end
   
-  def calculate_compoundedly
-    compoud_interest = @p * ((1 + @r) ** @t - 1)
+  def get_compound_interest
+    compoud_interest = @principal * ((1 + RATE) ** @time - 1)
   end
 
-  def compute_difference
-    "Difference between compound and simple interest for rate 10% p.a. = Rs.#{ @block.call(calculate_simply, calculate_compoundedly) }"
+  def print_difference
+    puts "Difference between compound and simple interest for rate 10% p.a. = Rs.#{ @block.call(get_simple_interest, get_compound_interest) }"
   end
 end
