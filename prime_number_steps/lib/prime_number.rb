a3e5f97869d7901ccd56d29f8d3bd76aabcdbdbb
Chhavi Khandelwal <chhavi@vinsol.com>
2013-12-09 16:29:23 +0530

    check prime function separated, code modified

--- prime_number_steps/lib/prime_number.rb
+++ prime_number_steps/lib/prime_number.rb
@@ -1,10 +1,22 @@
 require 'prime'
 class PrimeNumber
   def print_upto(limit)
-    2.step(limit, 1) do |number|
-      if number.prime?
-        yield number
+    2.step(limit, 1) do |i|
+      if prime_number?(i)
+        yield i
       end
     end
   end
+
+  def prime_number?(i)
+    flag = false
+    2.step(Math.sqrt(i).ceil, 1) do |j|
+      flag = i % j == 0 ? false : true
+      if flag == false
+        break
+      end
+    end
+    flag
+  end
+
 end
