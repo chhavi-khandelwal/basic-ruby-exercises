9fce58d6574db44717f0f4ce0e49b405f998c5b2
Chhavi Khandelwal <chhavi@vinsol.com>
2013-12-09 11:31:29 +0530

    File name changed, factorial logic separated, if-else removed

--- pascal_yield/lib/pascal_triangle_series.rb
+++ pascal_yield/lib/pascal_triangle_series.rb
@@ -1,22 +1,15 @@
 class PascalTriangleSeries
   def print_upto(limit)
-    1.upto(limit) do |i|
+    0.upto(limit) do |i|
       0.upto(i) do |j|
-        if i == j || j == 0
-          yield 1
-        else
-          n = (1..i).to_a.inject(:*)
-          r = (1..i-j).to_a.inject(:*)
-          n_r = (1..j).to_a.inject(:*)
-          yield n / (r * n_r)
-        end
+        i == j || j == 0 ? (yield 1) : (yield factorial(i) / (factorial(j) * factorial(i-j)))
       end
       print "\n"
     end
   end
 
-  def method_name
-    
+  def factorial(n)
+    factorial = (1..n).to_a.inject(:*)
   end
+
 end
-# iterators, change file name
