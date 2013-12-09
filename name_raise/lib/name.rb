200bd06800a95a1afcd6b69c4e712db5e7a9d393
Chhavi Khandelwal <chhavi@vinsol.com>
2013-12-09 15:38:47 +0530

    object initialized after validation, first letter validation corrected, access specifiers added, set modified

--- name_raise/lib/name.rb
+++ name_raise/lib/name.rb
@@ -1,20 +1,22 @@
+require_relative 'name_not_capitalized_exception'
+require_relative 'name_not_null_exception'
+
 class Name
 
   def initialize(firstname, secondname)
-    @firstname = firstname
-    @secondname = secondname
+    @firstname = validate_and_return(firstname, 'Firstname')
+    @secondname = validate_and_return(secondname, 'Secondname')
   end
 
-  def validate_and_return(name, set)
-    raise NameNotNullException, "#{ set } must not be null" if name.nil? || name.size == 0
-    raise NameNotCapitalizedException, 'First letter of the name must be capital' if name.capitalize != name && set == 'Firstname'
+  def validate_and_return(name, type)
+    raise NameNotNullException, "#{ type } must not be null" if name.nil? || name.size == 0
+    raise NameNotCapitalizedException, 'First letter of the name must be capital' if name[0].capitalize != name[0] && type == 'Firstname'
     name
   end
 
   def full_name
-    firstname = validate_and_return(@firstname, 'Firstname')
-    secondname = validate_and_return(@secondname, 'Secondname')
-    "Name: #{ firstname } #{ secondname }"
+    "Name: #{ @firstname } #{ @secondname }"
   end
 
+  private :validate_and_return
 end
