dd6efe7e2c4803062ca610cbcad955def2044920
Chhavi Khandelwal <chhavi@vinsol.com>
2013-12-09 12:57:26 +0530

    validation for principal and time added

--- interest_difference/bin/main.rb
+++ interest_difference/bin/main.rb
@@ -4,5 +4,11 @@ puts 'Enter the principal amount'
 principal = gets.chomp
 puts 'Enter the time'
 time = gets.chomp
-interest = Interest.new(principal, time) { |simple_interest, compoud_interest| (compoud_interest - simple_interest).round(3) }
-puts interest.compute_difference
+if principal.to_i <= 0
+  puts 'Enter a valid principal'
+elsif time.to_i <= 0
+  puts 'Enter a valid time-period'
+else
+  interest = Interest.new(principal, time) { |simple_interest, compoud_interest| (compoud_interest - simple_interest).round(3) }
+  interest.print_difference
+end
