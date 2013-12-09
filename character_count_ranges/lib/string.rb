7c04ed286a32d4055bbf7bc784d5b1bd43f39418
Chhavi Khandelwal <chhavi@vinsol.com>
2013-12-09 11:41:23 +0530

    ranges saved in variables

--- character_count_ranges/lib/string.rb
+++ character_count_ranges/lib/string.rb
@@ -1,12 +1,15 @@
 class String
   def count_character_type
     character_type_collection = Hash.new(0)
+    digits_range = '0'..'9'
+    lowercase_range = 'a'..'z'
+    uppercase_range = 'A'..'Z'
     self.each_char { |char| case char
-      when '0'..'9'
-       character_type_collection['digits'] += 1
-      when 'a'..'z'
+      when digits_range
+        character_type_collection['digits'] += 1
+      when lowercase_range
         character_type_collection['lowercase'] += 1
-      when 'A'..'Z'
+      when uppercase_range
         character_type_collection['uppercase'] += 1
       else
         character_type_collection['special-characters'] += 1
