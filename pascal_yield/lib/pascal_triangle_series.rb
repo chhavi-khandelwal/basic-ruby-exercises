3338eef3b7f8b9ddfb3632eaec37603951b81d43
Chhavi Khandelwal <chhavi@vinsol.com>
2013-12-10 11:21:21 +0530

    removed factorial variable

--- pascal_yield/lib/pascal_triangle_series.rb
+++ pascal_yield/lib/pascal_triangle_series.rb
@@ -2,7 +2,7 @@ class PascalTriangleSeries
   def print_upto(limit)
     0.upto(limit) do |i|
       0.upto(i) do |j|
-        i == j || j == 0 ? (yield 1) : (yield factorial(i) / (factorial(j) * factorial(i-j)))
+        i == j || j == 0 ? (yield 1) : (yield factorial(i) / (factorial(j) * factorial(i - j)))
       end
       print "\n"
     end
