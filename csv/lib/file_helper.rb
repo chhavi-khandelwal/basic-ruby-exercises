1915dbbc12f1546f70ce8efafdda1840efe0824c
Chhavi Khandelwal <chhavi@vinsol.com>
2013-12-10 15:31:32 +0530

    unrequitted methods removed

--- csv/lib/file_helper.rb
+++ csv/lib/file_helper.rb
@@ -2,31 +2,17 @@ require_relative 'employee'
 
 class FileHelper
 
-  def read_employee_record
-    employee = []
-    File.readlines('../data/employee.csv').each do |record|
-      name, emp_id, designation = record.split(', ')
-      employee << Employee.new(name, emp_id, designation)
-    end
-    employee
-  end
-
-  def write_employee_record
-    File.open('employee_data.txt', 'w') do |employee_file|
-      employee_record = store_employee_record
-      employee_record.each do |key, value|
-        employee_file.puts "\n#{ key }:"
-        value.each { |x| employee_file.puts x }
+  def write_employee_record(to_file, employee_record)
+    File.open(to_file, 'w') do |employee_file|
+      employee_record.each do |designation, employees|
+        if employees.length > 1
+          employee_file.puts "\n#{ designation + 's' }:"
+        else
+          employee_file.puts "\n#{ designation }:"
+        end
+        employee_file.puts employees
       end
     end
-  end
-
-  def store_employee_record
-    employee_record = Hash.new { |hash, key| hash[key] = [] }
-    read_employee_record.each do |employee|
-      employee_record[employee.designation + 's'] << "#{ employee.name } (EmpId:#{ employee.emp_id })"
-    end
-    employee_record
   end
 
 end
