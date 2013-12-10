1ea62fe3897594237a06d54bb738b86f8b912d2d
Chhavi Khandelwal <chhavi@vinsol.com>
2013-12-10 11:13:29 +0530

    file path added

--- factorial_with_exception/lib/integer.rb
+++ factorial_with_exception/lib/integer.rb
@@ -1,3 +1,6 @@
+require_relative '../lib/not_an_integer_exception'
+require_relative '../lib/negative_number_exception'
+
 class Integer
   def factorial
     raise NegativeNumberException, 'Negative Integer Error' if self < 0
