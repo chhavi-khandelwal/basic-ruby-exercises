8437edb97af947f09be9d01d203e1a48150466d9
Chhavi Khandelwal <chhavi@vinsol.com>
2013-12-10 15:27:42 +0530

    employee group_by logic implemented

--- csv/bin/main.rb
+++ csv/bin/main.rb
@@ -1,5 +1,9 @@
 require_relative '../lib/file_helper'
+require_relative '../lib/employee'
 
 file_helper = FileHelper.new
-file_helper.read_employee_record
-file_helper.write_employee_record
+employee_record = Employee.all(File.absolute_path('../data/employee.csv', File.dirname($0)))
+employee_record.shift
+employee_record = employee_record.group_by { |employee| employee.designation }
+
+file_helper.write_employee_record('employee_data.txt', employee_record.sort)
