253106c51df6fc434c7777f2d5d735b2a26e6d24
Chhavi Khandelwal <chhavi@vinsol.com>
2013-12-10 11:05:21 +0530

    attr_accessors added, string removed, unrequitted variables removed

--- interest_difference/lib/interest.rb
+++ interest_difference/lib/interest.rb
@@ -1,19 +1,19 @@
 class Interest
   RATE = 0.1
-
+  attr_reader :principal, :time, :block
   def initialize(principal, time, &block)
-    @principal, @time, @block = principal.to_i, time.to_i, block
+    @principal, @time, @block = principal.to_f, time.to_f, block
   end
 
   def get_simple_interest
-    simple_interest = @principal * RATE * @time
+    principal * RATE * time
   end
   
   def get_compound_interest
-    compoud_interest = @principal * ((1 + RATE) ** @time - 1)
+    principal * ((1 + RATE) ** time - 1)
   end
 
-  def print_difference
-    puts "Difference between compound and simple interest for rate 10% p.a. = Rs.#{ @block.call(get_simple_interest, get_compound_interest) }"
+  def difference
+    block.call(get_simple_interest, get_compound_interest)
   end
 end
