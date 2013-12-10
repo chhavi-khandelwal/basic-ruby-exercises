3e95394f9f48083eea4b8f17021930030b9178e5
Chhavi Khandelwal <chhavi@vinsol.com>
2013-12-10 15:29:58 +0530

    methods added, class variable added

--- csv/lib/employee.rb
+++ csv/lib/employee.rb
@@ -1,9 +1,22 @@
 class Employee
   attr_reader :designation, :emp_id, :name
+  @@employees = []
 
   def initialize(name, emp_id, designation)
     @name = name.chomp
     @emp_id = emp_id.chomp
     @designation = designation.chomp
   end
+
+  def self.all(from_file)
+    File.readlines(from_file).each do |record|
+      @@employees << Employee.new(*record.split(', '))
+    end
+    @@employees
+  end
+
+  def to_s
+    "#{ name } (EmpId:#{ emp_id })"
+  end
+
 end
