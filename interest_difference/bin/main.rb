a2b3c32144e4408ae4e789b749e8f805797d9d51
Chhavi Khandelwal <chhavi@vinsol.com>
2013-12-10 11:03:58 +0530

    interest difference string added

--- interest_difference/bin/main.rb
+++ interest_difference/bin/main.rb
@@ -4,11 +4,11 @@ puts 'Enter the principal amount'
 principal = gets.chomp
 puts 'Enter the time'
 time = gets.chomp
-if principal.to_i <= 0
+if principal.to_f <= 0
   puts 'Enter a valid principal'
-elsif time.to_i <= 0
+elsif time.to_f <= 0
   puts 'Enter a valid time-period'
 else
   interest = Interest.new(principal, time) { |simple_interest, compoud_interest| (compoud_interest - simple_interest).round(3) }
-  interest.print_difference
+  puts "Difference between compound and simple interest for rate 10% p.a. = Rs.#{ interest.difference }"
 end
