705f7a0d4b23820968ab616d8b58af6781fad074
Chhavi Khandelwal <chhavi@vinsol.com>
2013-12-10 16:30:46 +0530

    file removed

--- sum_time/lib/time.rb
+++
@@ -1,8 +0,0 @@
-class Time
-  def add_to(time2)
-    time2_in_secs = time2.hour * 3600 + time2.min * 60 + time2.sec
-    time = self + time2_in_secs
-    day = time.day - self.day
-    puts "(#{ self.hour }:#{ self.min }:#{ self.sec },#{ time2.hour }:#{ time2.min }:#{ time2.sec }) -> #{ day == 0 ? '' : "#{ day } day &" } #{ time.hour }:#{ time.min }:#{ time.sec }"
-  end
-end
