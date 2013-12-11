969f84e1099d88fa643f8457d147ff291cde44fa
Chhavi Khandelwal <chhavi@vinsol.com>
2013-12-11 13:59:40 +0530

    input method modified

--- sum_time_n_times/bin/main.rb
+++ sum_time_n_times/bin/main.rb
@@ -1,26 +1,27 @@
 require_relative '../lib/military_time'
 
 def get_valid_time
-  input_time = ''
-  loop do
-    input_time = gets.chomp
-    break if MilitaryTime.valid?(input_time)
-    puts 'Enter a valid time'
+  begin
+    military_time = MilitaryTime.new(gets.chomp)
+  rescue InvalidTimeException => message
+    puts message
+    retry
   end
-  input_time
+  military_time
 end
 
-def store_n_time
+def store_n_times
+  n_times = []
   loop do
-    MilitaryTime.new(get_valid_time)
+    n_times << get_valid_time
     puts 'Want to enter another?(y/n)'
     response = true if gets.chomp == 'n'
     break if response
   end
+  n_times
 end
 
-puts 'Enter no. of 24 hour time values you want to add'
-store_n_time
-net_time, day = MilitaryTime.sum
-MilitaryTime.all.each { |time| print "#{ time.hour }:#{ time.min }:#{ time.sec } "}
-print "-> #{ day == 0 ? '' : "#{ day } day & " }#{ net_time.hour }:#{ net_time.min }:#{ net_time.sec }\n"
+puts 'Enter time in 24 hour time format (h:m:s)'
+n_times = store_n_times
+net_time, day = MilitaryTime.sum(n_times)
+print "#{ n_times } -> #{ day == 0 ? '' : "#{ day } day & " }#{ net_time.hour }:#{ net_time.min }:#{ net_time.sec }\n"
