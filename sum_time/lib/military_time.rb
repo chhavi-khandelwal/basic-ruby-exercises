ae5235be005d815286bddb5579b8ce17ce20c12d
Chhavi Khandelwal <chhavi@vinsol.com>
2013-12-11 14:01:07 +0530

    validation added in initialize

--- sum_time/lib/military_time.rb
+++ sum_time/lib/military_time.rb
@@ -1,14 +1,27 @@
-class MilitaryTime
-  TIME_FORMAT = /^(?<hours>[01]?\d|2[0-3]):(?<mins>[0-5]?\d):(?<secs>[0-5]?\d)$/
+require 'time'
+require_relative 'invalid_time_exception'
 
-  def add_time(time1, time2)
+class MilitaryTime
+  FORMAT = /^(?<hours>[01]?\d|2[0-3]):(?<mins>[0-5]?\d):(?<secs>[0-5]?\d)$/
+  attr_reader :time
+  
+  def initialize(time)
+    raise InvalidTimeException, 'Invalid time' if !valid?(time)
+    @time = time
+  end
+
+  def self.add(time1, time2)
+    time1 = Time.parse(time1.time)
+    time2 = Time.parse(time2.time)
     time2_in_secs = time2.hour * 3600 + time2.min * 60 + time2.sec
     time = time1 + time2_in_secs
     day = time.day - time1.day
-    [day, time]
+    [time1, time2, day, time]
+  end
+  
+  private
+  def valid?(time)
+    time.match(FORMAT)
   end
 
-  def self.valid?(time)
-    time.match(TIME_FORMAT)
-  end
 end
