0c96f399076003fc0f9014e30d30f36c1d0f5b8d
Chhavi Khandelwal <chhavi@vinsol.com>
2013-12-11 15:12:11 +0530

    code separated and optimized

--- csv/bin/main.rb
+++ csv/bin/main.rb
@@ -1,9 +1,5 @@
 require_relative '../lib/file_helper'
 require_relative '../lib/employee'
 
-file_helper = FileHelper.new
-employee_record = Employee.all(File.absolute_path('../data/employee.csv', File.dirname($0)))
-employee_record.shift
-employee_record = employee_record.group_by { |employee| employee.designation }
-
-file_helper.write_employee_record('employee_data.txt', employee_record.sort)
+employee_records = Employee.all(File.absolute_path('../data/employee.csv', File.dirname(__FILE__)), headers = true)
+FileHelper.write_employee_records('employee_data.txt', employee_records.group_by { |employee| employee.designation }.sort)
