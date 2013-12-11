3971fee4ebafc0ac2f151cd4b9b29897021464ce
Chhavi Khandelwal <chhavi@vinsol.com>
2013-12-11 13:59:05 +0530

    input method modified

--- sum_time/bin/main.rb
+++ sum_time/bin/main.rb
@@ -1,20 +1,18 @@
 require_relative '../lib/military_time'
-require 'time'
 
 def get_valid_time
-  input_time = ''
-  loop do
-    input_time = gets.chomp
-    break if MilitaryTime.valid?(input_time)
-    puts 'Enter a valid time'
+  begin
+    military_time = MilitaryTime.new(gets.chomp)
+  rescue InvalidTimeException => message
+    puts message
+    retry
   end
-  Time.parse(input_time)
+  military_time
 end
 
-military_time = MilitaryTime.new
 puts 'Enter first time in 24 hour time format (h:m:s)'
 time1 = get_valid_time
 puts 'Enter second time in 24 hour time format (h:m:s)'
 time2 = get_valid_time
-day, time = military_time.add_time(time1, time2)
-puts "(#{ time1.hour }:#{ time1.min }:#{ time1.sec },#{ time2.hour }:#{ time2.min }:#{ time2.sec }) -> #{ day == 0 ? '' : "#{ day } day &" } #{ time.hour }:#{ time.min }:#{ time.sec }"
+time1, time2, day, sum_time = MilitaryTime.add(time1, time2)
+puts "(#{ time1.hour }:#{ time1.min }:#{ time1.sec },#{ time2.hour }:#{ time2.min }:#{ time2.sec }) -> #{ day == 0 ? '' : "#{ day } day &" } #{ sum_time.hour }:#{ sum_time.min }:#{ sum_time.sec }"
