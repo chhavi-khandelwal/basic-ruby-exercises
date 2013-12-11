00686772d6b72bdd9ff3141462414667656542b0
Chhavi Khandelwal <chhavi@vinsol.com>
2013-12-11 17:53:30 +0530

    code modified , method added

--- sales_tax_problem/bin/main.rb
+++ sales_tax_problem/bin/main.rb
@@ -1,23 +1,35 @@
-require_relative '../lib/string'
-require_relative '../lib/item_list'
-require_relative '../lib/item'
+require_relative '../lib/table'
 
-items = []
-loop do
-  print 'Name of the product: '
-  name = gets.chomp.strip 
-  name_flag = name.is_valid_name?
-  print 'Imported?[yes/no]: '
-  imported = true if gets.chomp.strip =~ /^yes$/i
-  print 'Exempted from sales tax[yes/no]? '
-  exemption = true if gets.chomp.strip =~ /^yes$/i
-  print 'Price:'
-  price = gets.chomp.strip
-  price_flag = price.is_valid_price?
-  items << Item.new(name, imported, exemption, price) if name_flag && price_flag
-  print 'Do you want to add more items to your list(y/n): '
-  break if gets.chomp.strip =~ /^n$/i
+def input_item_list
+  loop do
+    name = price = ''
+    begin
+      print 'Name of the product: '
+      name = gets.chomp.strip
+    end while !Item.valid_name?(name)
+
+    begin
+      print 'Imported?[yes/no]: '
+      imported = gets.chomp.strip
+    end  while imported !~ /^no|yes$/i
+
+    begin
+      print 'Exempted from sales tax[yes/no]? '
+      exemption = (gets.chomp.strip =~ /^yes$/i)
+    end while imported !~ /^no|yes$/i
+
+    begin
+      print 'Price:'
+      price = gets.chomp.strip
+    end while !Item.valid_price?(price)
+
+    Item.new(name, imported, exemption, price)
+
+    print 'Do you want to add more items to your list(y/n): '
+    break if gets.chomp.strip =~ /^n$/i
+  end
 end
 
-item_list = ItemList.new(items)
-item_list.display_items
+input_item_list
+table = Table.new
+table.display_items_grid
