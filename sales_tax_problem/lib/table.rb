a247e7e54b5f1c90f15de850a556347c9f749bd0
Chhavi Khandelwal <chhavi@vinsol.com>
2013-12-11 17:52:21 +0530

    code optimized

--- sales_tax_problem/lib/table.rb
+++ sales_tax_problem/lib/table.rb
@@ -1,22 +1,13 @@
+require_relative '../lib/item'
+
 class Table
+  HEADERS = ['Name', 'Imported', 'Exemption', 'Price', 'SubTotal']
+  STYLE = '|%-15s|%-15s|%-15s|%-15s|%-15s'
 
-  def initialize(headings, item_details, grand_total)
-    @headings = headings
-    @item_details = item_details
-    @grand_total = grand_total
-  end
-
-  def display_table
-    @headings.each do |heading|
-      print "|%-15s" % ["#{ heading }"]
-    end
-    @item_details.each do |item_detail|
-      item_detail.each do |detail|
-        print "|%-15s" % ["#{ detail }"]
-      end 
-      print "\n"
-   end
-   puts "GRAND-TOTAL = Rs. #{ @grand_total }"
+  def display_items_grid
+    print STYLE % HEADERS
+    puts Item.all
+    puts "GRAND-TOTAL = Rs. #{ Item.grand_total }"
   end
 
 end
