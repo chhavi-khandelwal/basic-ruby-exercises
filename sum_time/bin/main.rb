3c65bf6acd2f6345820844df69806ed97cac32c9
Chhavi Khandelwal <chhavi@vinsol.com>
2013-12-17 16:54:03 +0530

    method call changed

--- sum_time/bin/main.rb
+++ sum_time/bin/main.rb
@@ -14,5 +14,5 @@ puts 'Enter first time in 24 hour time format (h:m:s)'
 time1 = get_valid_time
 puts 'Enter second time in 24 hour time format (h:m:s)'
 time2 = get_valid_time
-day, sum_time = MilitaryTime.add(time1, time2)
-puts "(#{ time1 }, #{ time2 }) -> #{ day == 0 ? '' : "#{ day } day &" } #{ sum_time.hour }:#{ sum_time.min }:#{ sum_time.sec }"
+day, sum_time = time1 + time2
+puts "(#{ time1 }, #{ time2 }) -> #{ day == 0 ? '' : "#{ day } day &" } #{ sum_time.strftime("%H:%M:%S") }"
