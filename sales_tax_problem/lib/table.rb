ad40d320d128095b3cc447100d057137d2e04daf
Chhavi Khandelwal <chhavi@vinsol.com>
2013-12-19 12:32:57 +0530

    ouput formatted

--- sales_tax_problem/lib/table.rb
+++ sales_tax_problem/lib/table.rb
@@ -2,10 +2,10 @@ require_relative '../lib/item'
 
 class Table
   HEADERS = ['Name', 'Imported', 'Exemption', 'Price', 'SubTotal']
-  STYLE = '|%-15s|%-15s|%-15s|%-15s|%-15s'
+  FORMAT = '|%-15s|%-15s|%-15s|%-15s|%-15s'
 
   def display_items_grid
-    print STYLE % HEADERS
+    printf "#{ FORMAT % HEADERS } \n"
     puts Item.all
     puts "GRAND-TOTAL = Rs. #{ Item.grand_total }"
   end
