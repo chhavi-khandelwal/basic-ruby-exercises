f85d0a34136dc712a34f5d9f0a9385e3ec56c8ad
Chhavi Khandelwal <chhavi@vinsol.com>
2013-12-19 12:32:20 +0530

    constant name changed, calculation corrected

--- sales_tax_problem/lib/item.rb
+++ sales_tax_problem/lib/item.rb
@@ -3,16 +3,17 @@ class Item
   IMPORT_DUTY = 0.05
   VALID_PRICE_REGEXP = /^[+]?\d+(\.\d+)?$/
   VALID_NAME_REGEXP = /^[\w+\s?]+$/
-  STYLE = '|%-15s|%-15s|%-15s|%-15.2f|%-15.2f'
-  FIGURE = 2
+  FORMAT = '|%-15s|%-15s|%-15s|%-15.2f|%-15.2f'
+  PRECISION = 2
   @@items = []
   @@grand_total = 0
-  FLAG = 'yes'
+  TAX_FLAG = 'yes'
+
 
   def initialize(name, imported_status, sales_tax_exemption, price)
     @name = name
-    @imported = imported_status == FLAG
-    @sales_tax_exemption = sales_tax_exemption == FLAG
+    @imported = imported_status == TAX_FLAG
+    @sales_tax_exemption = sales_tax_exemption == TAX_FLAG
     @price = price.to_f
     @@items << self
     @@grand_total += sub_total
@@ -35,16 +36,16 @@ class Item
   end
 
   def to_s
-    STYLE % [@name, @imported, @sales_tax_exemption, @price, sub_total]
+    FORMAT % [@name, @imported, @sales_tax_exemption, @price, sub_total]
   end
 
   private
   def get_sales_tax
-    @sales_tax_exemption ? 0.00 : (@price * SALES_TAX).round(FIGURE)
+    @sales_tax_exemption ? 0.00 : (@price * SALES_TAX).round(PRECISION)
   end
 
   def get_import_duty
-    @imported ? (@price * IMPORT_DUTY).round(FIGURE) : 0.00
+    @imported ? (@price * IMPORT_DUTY).round(PRECISION) : 0.00
   end
 
   def sub_total
