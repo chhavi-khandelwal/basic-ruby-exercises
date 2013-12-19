9165a04f34f1df4c758c97cbbcf949b1a706fe32
Chhavi Khandelwal <chhavi@vinsol.com>
2013-12-19 12:30:05 +0530

    get_input_items modularized

--- sales_tax_problem/bin/main.rb
+++ sales_tax_problem/bin/main.rb
@@ -2,32 +2,46 @@ require_relative '../lib/table'
 
 def input_item_list
   loop do
-    name = price = ''
-    begin
-      print 'Name of the product: '
-      name = gets.chomp.strip
-    end while !Item.valid_name?(name)
-
-    begin
-      print 'Imported?[yes/no]: '
-      imported = gets.chomp.strip
-    end  while imported !~ /^no|yes$/i
-
-    begin
-      print 'Exempted from sales tax[yes/no]? '
-      exemption = (gets.chomp.strip =~ /^yes$/i)
-    end while imported !~ /^no|yes$/i
-
-    begin
-      print 'Price:'
-      price = gets.chomp.strip
-    end while !Item.valid_price?(price)
-
+    name = product_name
+    imported = product_imported_status
+    exemption = product_sales_tax_status
+    price = product_price
     Item.new(name, imported, exemption, price)
-
     print 'Do you want to add more items to your list(y/n): '
     break if gets.chomp.strip =~ /^n$/i
   end
+end
+
+def product_name
+  begin
+    print 'Name of the product: '
+    name = gets.chomp.strip
+  end while !Item.valid_name?(name)
+  name
+end
+
+def product_price
+  begin
+    print 'Price:'
+    price = gets.chomp.strip
+  end while !Item.valid_price?(price)
+  price
+end
+
+def product_imported_status
+  begin
+    print 'Imported?[yes/no]: '
+    imported = gets.chomp.strip
+  end  while imported !~ /^no|yes$/i
+  imported
+end
+
+def product_sales_tax_status
+  begin
+    print 'Exempted from sales tax[yes/no]? '
+    exemption = gets.chomp.strip
+  end while exemption !~ /^no|yes$/i
+  exemption
 end
 
 input_item_list
