5fd38bd96d6a4d5fd2459bf36c74768fe7eaa577
Chhavi Khandelwal <chhavi@vinsol.com>
2013-12-20 15:08:55 +0530

    corrected argument assignment

--- csv/bin/main.rb
+++ csv/bin/main.rb
@@ -1,5 +1,5 @@
 require_relative '../lib/file_helper'
 require_relative '../lib/employee'
 
-employee_records = Employee.all(File.absolute_path('../data/employee.csv', File.dirname(__FILE__)), headers = true)
+employee_records = Employee.all(File.absolute_path('../data/employee.csv', File.dirname(__FILE__)), true)
 FileHelper.write_employee_records('employee_data.txt', employee_records.group_by { |employee| employee.designation }.sort)
